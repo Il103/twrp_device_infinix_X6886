baeb3a47be97bd122eb00d0a6350f0f0c7f00024
B E R U <berucrypt02@gmail.com>
2026-07-03 22:45:26 +0300

    Add files via upload

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2024 OrangeFox Recovery Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo ofox_X6886-eng
+add_lunch_combo ofox_X6886-userdebug
+add_lunch_combo ofox_X6886-user
