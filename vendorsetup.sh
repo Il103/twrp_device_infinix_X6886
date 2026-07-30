9bd18365c037deb94d899abbeb6eb7d9bedcaa3b
# B E R U <berucrypt02@gmail.com>
2026-07-31 00:42:34 +0300

    Delete vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2024 OrangeFox Recovery Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-add_lunch_combo ofox_X6886-eng
-add_lunch_combo ofox_X6886-userdebug
-add_lunch_combo ofox_X6886-user
